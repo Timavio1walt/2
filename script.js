f652a69061c2f60662f5118623809f9bdef7e0a3
Timavio1walt <timurbagautdinov21@gmail.com>
2025-06-22 16:04:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const vopros1 = document.querySelector(".vopros1")
+const panel1 = document.querySelector(".panel1")
+vopros1.addEventListener("click", () =>
+{
+    vopros1.classList.toggle("active")
+    panel1.classList.toggle("open")
+})
+const vopros2 = document.querySelector(".vopros2")
+const panel2 = document.querySelector(".panel2")
+vopros2.addEventListener("click", () =>
+{
+    vopros2.classList.toggle("active")
+    panel2.classList.toggle("open")
+})
+const vopros3 = document.querySelector(".vopros3")
+const panel3 = document.querySelector(".panel3")
+vopros3.addEventListener("click", () =>
+{
+    vopros3.classList.toggle("active")
+    panel3.classList.toggle("open")
+})
+const vopros4 = document.querySelector(".vopros4")
+const panel4 = document.querySelector(".panel4")
+vopros4.addEventListener("click", () =>
+{
+    vopros4.classList.toggle("active")
+    panel4.classList.toggle("open")
+})
